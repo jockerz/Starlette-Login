837f9174f3efbd5624cd0358b60e7f182a4e26c3
user <user@localhost.localdomain>
2024-11-08 15:08:25 +0700

    minor bug fix

--- starlette_login/backends.py
+++ starlette_login/backends.py
@@ -15,7 +15,7 @@ class SessionAuthBackend(AuthenticationBackend):
 
     async def authenticate(
         self, conn: HTTPConnection
-    ) -> tuple[AuthCredentials, t.Union[AnonymousUser, UserMixin]]:
+    ) -> t.Tuple[AuthCredentials, t.Union[AnonymousUser, UserMixin]]:
         # Load user from session
         config = self.login_manager.config
         remember_cookie = config.REMEMBER_COOKIE_NAME
